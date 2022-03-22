06d57e4d400d6f910a27104947755e6b2d264a54
tama-biro <samuel.thelaus@gmail.com>
2022-03-22 07:57:44 +0000

    Descriptives added

--- analysis.R
+++ analysis.R
@@ -11,10 +11,25 @@ data <- read.csv(file.choose())
 
 #### Post-low AE ####
 
+# Descriptives
+desc_data <- data %>%
+  filter(type != 'Diversion' & results > 0) %>%
+  group_by(ID, condition, experiment) %>%
+  summarize(post_low_c = 
+              mean(results[stddev_1 < stddev_2]) - 
+              mean(results[type == 'Control']),
+            post_high_c = 
+              mean(results[stddev_1 > stddev_2]) - 
+              mean(results[type == 'Control'])) %>%
+  ungroup() %>%
+  select(experiment, post_low_c, post_high_c)
+
+psych::describeBy(desc_data$post_high_c, group = desc_data$experiment)
+
 # Test 1
 # After-effect in T1 vs. T2
 data_t1 <- data %>%
-  filter(type != 'Diversion' & results > 0) %>%
+  filter(type != 'Diversion' & results > 0 & experiment %in% c('', 'S5')) %>%
   group_by(ID, condition) %>%
   summarize(post_low_c = 
               mean(results[stddev_1 < stddev_2]) - 
@@ -25,13 +40,22 @@ data_t1 <- data %>%
   ungroup() %>%
   mutate(condition = factor(condition, levels = c('T2', 'T1')))
 
+
+
 # Check normality (holds for both)
-hist(data_t1$post_high_c[data_t1$condition == 'T1'])
-shapiro.test(data_t1$post_high_c[data_t1$condition == 'T1'])
+hist(data_t1$post_high_c[data_t1$condition == 'T2'])
+shapiro.test(data_t1$post_high_c[data_t1$condition == 'T2'])
 
 psych::describe((data_t1$post_high_c[data_t1$condition == 'T2']))
 
-data_t1$post_high_bc <- boxcoxTransform(data_t1$post_high_c+5, lambda = 3)
+out_bc <- boxcox(data_t1$post_high_c+5, objective = 'Log-Likelihood',
+                 lambda = seq(-10, 10))
+
+which(out_bc$objective == max(out_bc$objective, na.rm = TRUE))
+
+lmbda <- out_bc$lambda[14]
+
+data_t1$post_high_bc <- boxcoxTransform(data_t1$post_high_c+5, lambda = lmbda)
 
 sum(data_t1$post_high_c[data_t1$condition == 'T1'] < 0)/
   length(data_t1$post_high_c[data_t1$condition == 'T1'])
@@ -201,7 +225,7 @@ ggsave('difference_postlow_3_T1.png', height = 6, width = 12)
 
 # T-test
 data_int_t <- data %>%
-  filter(type != 'Diversion' & results > 0) %>%
+  filter(type != 'Diversion' & results > 0 & experiment %in% c('', 'S5')) %>%
   group_by(ID, condition) %>%
   summarize(after_effect = -(mean(results[stddev_1 > stddev_2]) - 
                           mean(results[type == 'Control'])) -
@@ -221,9 +245,11 @@ d_c <- (mean(data_int_t$after_effect[data_int_t$condition == 'T1']) -
     )
     /2)
 
+pwr.t2n.test(n1 = 56, n2 = 31, d = 0.5, alternative = 'greater')
+
 # Mixed model
 data_int <- data %>%
-  filter(type != 'Diversion' & results > 0) %>%
+  filter(type != 'Diversion' & results > 0 & experiment != 'S2') %>%
   group_by(ID) %>%
   mutate(after_effect = if_else(stddev_1 < stddev_2 | stddev_1 > stddev_2, 
                                 results - mean(results[type == 'Control']),
@@ -268,19 +294,24 @@ ggsave('after-effect_difference.png', width = 10, height = 8)
 
 #### Power for mixed models ####
 power = c()
+power_int = c()
 for(i in 1:100) {
   sim_arguments <- list(
-    formula = y ~ 1 + condition + (1 | id),
-    fixed = list(condition = list(var_type = 'factor',
-                              levels = c('T1', 'T2'),
-                              var_level = 2)),
-    # fixed = list(condition = list(var_type = 'continuous',
-    #                               mean = 0,
-    #                               sd = 1,
-    #                               var_level = 2)),
+    formula = y ~ 1 + condition * trial_type + (1 | id),
+    # fixed = list(condition = list(var_type = 'factor',
+    #                           levels = c('T1', 'T2'),
+    #                           var_level = 2)),
+    fixed = list(
+    condition = list(var_type = 'factor',
+                     levels = c('T1', 'T2'),
+                     var_level = 2),
+    trial_type = list(var_type = 'factor',
+                      levels = c('post-low', 'post-high'),
+                      var_level = 2)
+    ),
     randomeffect = list(int_id = list(variance = 1, var_level = 2)),
-    sample_size = list(level1 = 9, level2 = 186),
-    reg_weights = c(0, -.2)
+    sample_size = list(level1 = 18, level2 = 165),
+    reg_weights = c(0, -.5, -.5, -.5)
   )
   
   nested_data <- sim_arguments %>%
@@ -289,19 +320,28 @@ for(i in 1:100) {
     simulate_error(sim_arguments) %>%
     generate_response(sim_arguments)
   
-  model = lmer(y ~ 1 + condition+ (1 | id),
+  model = lmer(y ~ 1 + condition * trial_type + (1 | id),
                 data = nested_data)
   
   t = summary(model)[[10]][2,4]
   
-  if(t < -1.68) {
+  if(abs(t) > 1.96) {
     power[length(power) + 1] = 1
   } else {
     power[length(power) + 1] = 0
   }
+  
+  t_int = summary(model)[[10]][4,4]
+  
+  if(abs(t_int) > 1.96) {
+    power_int[length(power_int) + 1] = 1
+  } else {
+    power_int[length(power_int) + 1] = 0
+  }
 }
 
 mean(power)
+mean(power_int)
 
 
 
